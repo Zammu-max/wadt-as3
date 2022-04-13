d462d745a29dab9504673a6bb9f1952124b4b14c
Zam <20FTT1443@student.pb.edu.bn>
2022-04-13 21:40:57 +0800

    description added

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,11 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <img src="./assets/img1.jpeg" alt="authors with friends">
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <img id="imgOne" src="./assets/img1.jpeg" alt="authors with friends">
+  <p id="desc">Hello my name is Muazzam and im currently studying Web Development in Politeknik Brunei. In my free time i like to workout, play games and learn how to code. </p>
+  <br><br><br>
+  <HelloWorld msg="Powered Vue 3 + Vite" />
 </template>
 
 <style>
@@ -18,5 +20,18 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#imgOne{
+  margin-left: auto;
+  margin-right: auto;
+  width: 400px;
+  height: 350px;
+}
+
+#desc{
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
 }
 </style>
