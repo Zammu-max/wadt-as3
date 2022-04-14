1dc2a67a2302d591a5d8e97d58900a83289fea0f
Zam <20FTT1443@student.pb.edu.bn>
2022-04-14 12:37:33 +0800

    modify app. vue

--- src/App.vue
+++ src/App.vue
@@ -75,7 +75,7 @@ import { MenuIcon, XIcon } from '@heroicons/vue/outline'
             <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
               <span class="block xl:inline">Muazzam Murni</span> <br>
               {{ ' ' }}
-              <span class="block text-indigo-600 xl:inline">20FTT1443</span>
+              <span class="block text-indigo-600 xl:inline">Bahit was here</span>
             </h1>
             <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">Hello im Muazzam, currently studying Web Development and this is my Portfolio.</p>
             <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">I am really interested in learning Web Development and in my free time i like to learn how to code.</p>
