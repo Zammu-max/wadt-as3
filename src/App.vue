918da9f079b089b727851039f5bf3e10008f76d5
Zam <20FTT1443@student.pb.edu.bn>
2022-04-14 11:08:37 +0800

    updated desc

--- src/App.vue
+++ src/App.vue
@@ -73,11 +73,13 @@ import { MenuIcon, XIcon } from '@heroicons/vue/outline'
         <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
           <div class="sm:text-center lg:text-left">
             <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-              <span class="block xl:inline">Portfolio</span> <br>
+              <span class="block xl:inline">Muazzam Murni</span> <br>
               {{ ' ' }}
-              <span class="block text-indigo-600 xl:inline">Muazzam Murni</span>
+              <span class="block text-indigo-600 xl:inline">20FTT1443</span>
             </h1>
             <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">Hello im Muazzam, currently studying Web Development and this is my Portfolio.</p>
+            <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">I am really interested in learning Web Development and in my free time i like to learn how to code.</p>
+            <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">For any inquiries, contact me on my email: 20FTT1443@student.pb.edu.bn</p>
             <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
               <!-- <div class="rounded-md shadow">
                 <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"> Get started </a>
@@ -99,12 +101,12 @@ import { MenuIcon, XIcon } from '@heroicons/vue/outline'
 <script>
 
 
-// const navigation = [
-//   { name: 'Product', href: '#' },
-//   { name: 'Features', href: '#' },
-//   { name: 'Marketplace', href: '#' },
-//   { name: 'Company', href: '#' },
-// ]
+const navigation = [
+  { name: 'Home', href: '#' },
+  // { name: 'Features', href: '#' },
+  // { name: 'Marketplace', href: '#' },
+  // { name: 'Company', href: '#' },
+]
 
 export default {
   components: {
